97793612c55d222ad1ca173fc2187c2a76121b59
Ghilain <ishimweghilain5050@gmail.com>
2022-09-21 18:21:16 +0200

    add validation and spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base # rubocop:todo Style/Documentation
   has_many :comments
   has_many :likes
   after_save :update_posts_counter
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   private
 
